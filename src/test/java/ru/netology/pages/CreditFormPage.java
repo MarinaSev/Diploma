be1faa8aecee8cfaf3b5a98f810e5ba116e81cd6
MarinaSev <pawpawsap@gmail.com>
2022-03-02 17:03:23 +0300

    soft settings

---
+++ src/test/java/ru/netology/pages/CreditFormPage.java
@@ -0,0 +1,5 @@
+package ru.netology.pages;
+
+public class CreditFormPage {
+
+}
---
+++ src/test/java/ru/netology/test/TourPageTest.java
@@ -0,0 +1,21 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+
+public class TourPageTest {
+
+    @Test
+    public void shouldOpenBuyingForm() {
+        //открыть страицу
+        //ажать копку <Купить>
+        //адпись "Оплата по карте" в аличии
+    }
+
+    @Test
+    public void shouldOpenCreditForm() {
+        //открыть страицу
+        //ажать копку <Купить в крдит>
+        //адпись "Кредит по данным карты" в аличии
+    }
+
+}
